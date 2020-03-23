fa25c2a0dabb51d2fe3eb968fdfb6b3d4c3a60e7
hanbin <jjck5938@gmail.com>
2020-03-23 16:47:22 +0900

    0323 model up

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -26,8 +26,9 @@ class encoder_(nn.Module):
         self.lowest_dense_res = 128 // 16
         self.dims = self.nc * self.d_ch_dims
 
- 
-        self.encoder = nn.Sequential(
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.encoder1 = nn.Sequential(
         #3*128*128
         nn.Conv2d(self.nc, self.nef, kernel_size=4, stride=2, padding=1, bias=False),
         nn.LeakyReLU(0.2, inplace=True),
@@ -39,25 +40,37 @@ class encoder_(nn.Module):
         nn.Conv2d(self.nef*2, self.nef*4, kernel_size=4, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(self.nef*4),
         nn.LeakyReLU(0.2, inplace=True),
-        #512*16*16
         nn.Conv2d(self.nef*4, self.nef*8, kernel_size=4, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(self.nef*8),
+        nn.LeakyReLU(0.2, inplace=True)
+        #512*16*16
+        )
+        self.encoder2 = nn.Sequential(
+        nn.Conv2d(self.nef*8, self.nef*4, kernel_size=3, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(self.nef*4),
         nn.LeakyReLU(0.2, inplace=True),
         #1024*8*8
-        nn.Conv2d(self.nef*8, self.nef*16, kernel_size=4, stride=2, padding=1, bias=False),
-        nn.BatchNorm2d(self.nef*16),
+        nn.Conv2d(self.nef*4, self.nef*8, kernel_size=3, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(self.nef*8),
         nn.LeakyReLU(0.2, inplace=True),
         #2048*4*4
+        )
+        self.encoder3 = nn.Sequential(
         nn.Flatten(),
-        nn.Linear(2048*4*4, 2048),
+        nn.Linear(1024*8*8, 512),
         nn.LeakyReLU(0.2, inplace=True),
-        nn.Linear(2048, 4*4*2048),
-        nn.LeakyReLU(0.2, inplace=True)
+        nn.Linear(512, 8*8*512),
+        nn.LeakyReLU(0.2, inplace=True),
+
         )
 
     def forward(self, x):
-        x= self.encoder(x) 
-        x = x.view(-1,2048,4,4)
+        x = self.encoder1(x)
+        residual1 = x
+        x = self.encoder2(x)
+        x = self.relu1(x+residual1)
+        x = self.encoder3(x)
+        x = x.view(-1,512,8,8)
         return x
 
 
@@ -79,14 +92,19 @@ class decoder_(nn.Module):
 
 
 
-        self.decoder  = nn.Sequential(
-        nn.ConvTranspose2d(self.nef*16, self.nef*8, kernel_size=4, stride=2, padding=1, bias=False),
-        nn.BatchNorm2d(self.nef*8),
-        nn.LeakyReLU(0.1, inplace=True),
-        nn.ConvTranspose2d(self.nef*8, self.nef*4, kernel_size=4, stride=2, padding=1, bias=False),
+        self.decoder1  = nn.Sequential(
+        nn.ConvTranspose2d(self.nef*4, self.nef*4, kernel_size=3, stride=1, padding=1, bias=False),
         nn.BatchNorm2d(self.nef*4),
         nn.LeakyReLU(0.1, inplace=True),
+        nn.ConvTranspose2d(self.nef*4, self.nef*4, kernel_size=3, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(self.nef*4),
+        nn.LeakyReLU(0.1, inplace=True)
+        )
+        self.decoder2 = nn.Sequential(
         nn.ConvTranspose2d(self.nef*4, self.nef*2, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(self.nef*2),
+        nn.LeakyReLU(0.1, inplace=True),
+        nn.ConvTranspose2d(self.nef*2, self.nef*2, kernel_size=4, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(self.nef*2),
         nn.LeakyReLU(0.1, inplace=True),
         nn.ConvTranspose2d(self.nef*2, self.nef, kernel_size=4, stride=2, padding=1, bias=False),
@@ -96,7 +114,9 @@ class decoder_(nn.Module):
         )
         
     def forward(self, x):
-        x= self.decoder(x)
+        residual = x
+        x = self.decoder1(x)
+        x = self.decoder2(x+residual)
         return x
 
 class autoencoder(nn.Module):
